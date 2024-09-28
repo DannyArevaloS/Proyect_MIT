7f9660268413019c32b1604c4210929f60dc5a65
DannyArevaloS <darevalo_dev@outlook.es>
2024-09-28 16:46:37 +0200

    cambio 4

--- includes/main.php
+++ includes/main.php
@@ -12,7 +12,7 @@
             </article>
             <article class="entradas">
                 <a href="">
-                <h2>Titulo de mi entrada</h2>
+                <h2>Section</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ligula in magna consectetur interdum. Pellentesque nec lacus mauris. Curabitur et commodo erat, ac convallis orci. Donec tempor dolor neque, vel tempor sem ultrices quis. Sed tempus orci felis, nec luctus sapien auctor ut. Vivamus semper velit ut accumsan rutrum. Curabitur tincidunt tortor at tempus laoreet. Praesent non bibendum metus, quis consequat neque. Nam ultrices augue nec lacinia venenatis. Sed et laoreet enim, sed posuere metus. Fusce vitae ornare urna. Suspendisse sed nisl ipsum. Donec feugiat egestas nunc, quis lacinia lectus elementum cursus. Nam a bibendum lacus. Maecenas a lacus tincidunt, lobortis felis id, eleifend turpis. Mauris id urna vitae odio imperdiet vestibulum ultricies at est.</p>
                 </a>
                 
